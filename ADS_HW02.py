3c632be9ada76116e898f88b289cac0144ce70d0
William Wan <william10000@gmail.com>
2015-07-18 14:32:20 -0400

    tested approximate matching using k-mer index

--- ADS_HW02.py
+++ ADS_HW02.py
@@ -9,6 +9,8 @@ Created on Sun Jul 12 06:30:51 2015
 
 # import previously written Boyer-Moore preprocessing algorithm written
 import bm_preproc as bm
+import bisect
+import sys
 
 def readGenome(filename):
     genome = ''
@@ -59,13 +61,69 @@ def boyer_moore(p, p_bm, t):
         aligns += 1
     return occurrences, comp, aligns
 
+class Index(object):
+    def __init__(self, t, k):
+        ''' Create index from all substrings of size 'length' '''
+        self.k = k  # k-mer length (k)
+        self.index = []
+        for i in range(len(t) - k + 1):  # for each k-mer
+            self.index.append((t[i:i+k], i))  # add (k-mer, offset) pair
+        self.index.sort()  # alphabetize by k-mer
+    
+    def query(self, p):
+        ''' Return index hits for first k-mer of P '''
+        kmer = p[:self.k]  # query with first k-mer
+        i = bisect.bisect_left(self.index, (kmer, -1))  # binary search
+        hits = []
+        while i < len(self.index):  # collect matching index entries
+            if self.index[i][0] != kmer:
+                break
+            hits.append(self.index[i][1])
+            i += 1
+        return hits
+
+def queryIndex(p, t, index):
+    k = index.k # k-mer size used to create the index
+    offsets = []
+    for i in index.query(p):
+        if p[k:] == t[i+k:i+len(p)]:  # verify that rest of P matches
+            offsets.append(i)
+    return offsets
+
+def approximate_match_index(p, t, k, hits, mm):
+    occurrences = []
+    for i in hits:
+        mismatches = 0
+        for j, mer in enumerate(p[k:]):
+            print(j)
+            print(mer)
+            if mer != t[i+k+j]:
+                mismatches += 1
+                print('mismatch')
+            if mismatches > mm:
+                break
+        if mismatches <=mm:
+            occurrences.append(i)
+    return occurrences
 
 # test cases
 t = 'ATGCCTTGCA'
 p = 'GCCT'
+t2 = 'ATGCTAGCTACGTTACGT'
+p2 = 'GCT'
 p_bm = bm.BoyerMoore(p, alphabet='ACGT')
-t1 = boyer_moore(p, p_bm, t)
+p2_bm = bm.BoyerMoore(p2, alphabet='ACGT')
+test1 = boyer_moore(p, p_bm, t)
+test2 = boyer_moore(p2, p2_bm, t2)
+test2n = naive(p2, t2)
 # output of  - naive(p, t) is ([2], 10, 7) - seems correct
+
+t = 'ACTTGGAGATCTTTGAGGCTAGGTATTCGGGATCGAAGCTCATTTCGGGGATCGATTACGATATGGTGGGTATTCGGGA'
+p = 'GGTATTCGGGA'
+index3 = Index(t, 3)
+print(queryIndex(p, t, index3))
+print(approximate_match_index(p, t, index3.k, index3.query(p), 7))
+# end test cases
 
 
 
@@ -94,6 +152,23 @@ q3 = boyer_moore(p3, p3_bm, genome)
 
 # 1st attempt ([56922], 165191, 127974)
 
+
+# Question #4 - 
+# Write a function that, given a length-24 pattern P and given an Index object built 
+# on 8-mers, finds all approximate occurrences of P within T with up to 2 mismatches. 
+# Insertions and deletions are not allowed.
+
+# How many times does the string GGCGCGGTGGCTCACGCCTGTAAT 
+#( derived from human Alu sequences) occur with up to 2 substitutions in the excerpt of 
+# human chromosome 1? Hint: multiple index hits might direct you to the same match 
+# multiple times; be careful not to count a match more than once.
+genomeIndex = Index(genome, 8)
+p4 = 'GGCGCGGTGGCTCACGCCTGTAAT'
+
+print(queryIndex(p, t, index3))
+print(approximate_match_index(p, t, index3.k, index3.query(p), 7))
+
+# no more than 13 times based on k-mer indexing
 
 
 
